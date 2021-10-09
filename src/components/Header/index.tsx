fd738b2376a26eb4ff1ce5f0796d86f82eebff7d
Leonardo <leonardo.pb@icloud.com>
2021-10-09 20:18:35 -0300

    feat: Adding Header Component and favicon

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import styles from './styles.module.scss';
+
+export function Header() {
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="ig news logo" />
+                <nav>
+                    <a href="#">
+                        <a className={styles.active}>Home</a>
+                        <a>Posts</a>
+                    </a>
+                </nav>
+            </div>
+        </header>
+    )
+}
