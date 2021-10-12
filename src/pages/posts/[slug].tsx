3b0eec1beb779caab381066e6b0246d2c70c1030
Leonardo <leonardo.pb@icloud.com>
2021-10-12 17:59:14 -0300

    redirecting to preview page

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -42,7 +42,7 @@ export const getServerSideProps: GetServerSideProps = async({ req, params }) => 
     if(!session?.activeSubscription) {
         return {
             redirect: {
-                destination: '/',
+                destination: `/preview/${slug}`,
                 permanent: false,
             }
         }
